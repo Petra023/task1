ad31ade9dc9c86f80d65803aa11852a6dd36fd4e
Artur <atur777@mail.ru>
2017-09-11 22:52:59 +0300

    add week 3 task 1 im type .c

--- week3/binary_search.c
+++ week3/binary_search.c
@@ -1,11 +1,11 @@
 bool search(int value, int values[], int n)
 {
     int min, max, mid;
-    int count_min = 0; 
+    int count_min = 0;
     int count_max = n - 1;
     int count_mid = (count_max + count_min)/2;
     int m = 0; //счетчик количества итераций (ради интереса)
-    bool stop = false;
+    bool needle1 = false;
 
     min = values[count_min];
     max = values[count_max];
@@ -19,32 +19,7 @@ bool search(int value, int values[], int n)
         m++;
 
 
-        if ((count_max - count_min) == 1)
-        {
-            if (max == value)
-            {
-                stop = true;
-                printf("max=\n");
-
-            }
-
-            else if (min == value)
-            {
-                stop = true;
-                printf("min=\n");
-
-            }
-
-
-            else
-            {
-                stop = false;
-                printf("!=\n");
-                break;
-            }
-        }
-
-        else if (value > mid)
+        if (value > mid)
         {
             count_min = count_mid;
             min = values[count_min];
@@ -53,26 +28,40 @@ bool search(int value, int values[], int n)
             printf(">mid\n");
         }
 
-        else if (value < mid)
+        if (value < mid)
         {
             count_max = count_mid;
             max = values[count_max];
             count_mid = (count_max + count_min)/2;
             mid = values[(count_max + count_min)/2];
             printf("<mid\n");
-
         }
 
         if (mid == value)
-            {
-            stop = true;
+        {
+            needle1 = true;
             printf("=mid\n");
+        }
 
-            }
+        if (max == value)
+        {
+            needle1 = true;
+            printf("=max\n");
+        }
 
-    } while (mid != value || ((count_max - count_min) == 1 && max == value) || ((count_max - count_min) == 1 && min == value));
+        if (min == value)
+        {
+            needle1 = true;
+            printf("=min\n");
+        }
+
+        if ((count_max - count_min) == 1)
+        {
+            needle1 = false;
+            printf("!=\n");
+        }
+    } while (min != value && max != value && mid != value && count_max - count_min != 1);
 
     printf("%i\n", m);
-    return stop;
-
+    return needle1;
 }
